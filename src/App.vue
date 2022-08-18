5dc455de10364cce629fc66ffc9334a0bf49b633
panda1 <blueblue0011@gmail.com>
2022-08-18 15:12:28 -0700

    stuck on 5.1, will come back to it

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <toast-notification></toast-notification>
+   <delete-button></delete-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DeleteButton from '@/components/DeleteButton.vue';
+import ToastNotification from '@/components/ToastNotification.vue';
+
+
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    ToastNotification,
+    DeleteButton
+  },
+};
+
 </script>
 
 <style>
---
+++ src/components/ToastNotification.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'toast-notification'
+    }
+</script>
+
+<style scoped>
+
+</style>
